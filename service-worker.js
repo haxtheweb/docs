16ef52caf0236bc49da8728281ae1b45fecd0383
www-data <www-data@hax.vmhost.psu.edu>
2024-07-09 10:29:52 -0400

    Page updated: Gallery Exhibits (item-d7a7736b-3262-4d0e-8df7-a9af7823a2b7)

--- service-worker.js
+++ service-worker.js
@@ -156,7 +156,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-d7a7736b-3262-4d0e-8df7-a9af7823a2b7\/index.html",
-        "QuRjxun0im6Xwz9bESimSg"
+        "CbwBZt7Pm5EWgIOdaSfR0A"
     ],
     [
         "pages\/item-73d866cd-ebd0-4124-ad99-b1b18455327d\/index.html",
