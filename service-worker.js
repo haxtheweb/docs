61b1d5ccf89bb21c7afbe5193c94dedd8d0713b0
www-data <www-data@hax.vmhost.psu.edu>
2024-07-12 14:39:53 -0400

    Page updated: Follow Cognitive Load Theory (item-de9dbbb5-949b-49c0-95ad-dbbc84835db0)

--- service-worker.js
+++ service-worker.js
@@ -120,7 +120,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-de9dbbb5-949b-49c0-95ad-dbbc84835db0\/index.html",
-        "en5XSupEXekIaIByEUJttA"
+        "IHRKWzcSL7UO48BdmH9czQ"
     ],
     [
         "pages\/item-a1b1587a-b40a-463f-ad4a-e5a05a98fd8c\/index.html",
