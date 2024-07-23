7f984b5c5bfa70c7695ab97ed8fc0b1ac49346bf
www-data <www-data@hax.vmhost.psu.edu>
2024-07-23 11:06:26 -0400

    Page updated: Planning the Lecture (item-a1b1587a-b40a-463f-ad4a-e5a05a98fd8c)

--- service-worker.js
+++ service-worker.js
@@ -264,7 +264,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-a1b1587a-b40a-463f-ad4a-e5a05a98fd8c\/index.html",
-        "9LH4tLdAJmS1uYogyMebw"
+        "yEFE2kuqn01JQVsCsK4F7A"
     ],
     [
         "pages\/item-4d1a84b2-ddcb-4dbc-9b37-2d135442c32d\/index.html",
