559bb46b93c91be8156bff17a3ef53a9cc4d0055
www-data <www-data@hax.vmhost.psu.edu>
2024-07-24 10:10:25 -0400

    Page updated: Foster Student Engagement (item-3c1bf773-d9e2-4658-84f8-6ab5e3f42ab2)

--- service-worker.js
+++ service-worker.js
@@ -296,7 +296,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-3c1bf773-d9e2-4658-84f8-6ab5e3f42ab2\/index.html",
-        "OpmkoF1ZJDn8l4qkD8HsVQ"
+        "ww9s5j6og4TFpyk2SbqrnA"
     ],
     [
         "pages\/item-a54cdc47-2f1c-4d82-8baf-fce180194a81\/index.html",
