31184bee323e57a7fc025a54ae513164e7819b46
www-data <www-data@hax.vmhost.psu.edu>
2024-07-25 11:02:26 -0400

    Page updated: Multiple choice (item-3c4f0dbd-ef6a-4c8d-acaa-27b295243948)

--- service-worker.js
+++ service-worker.js
@@ -172,7 +172,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-3c4f0dbd-ef6a-4c8d-acaa-27b295243948\/index.html",
-        "qwf8PzCmQnb8jcXDdzY6Q"
+        "vmX2ExxzbNZvgdrRbXBTQQ"
     ],
     [
         "pages\/item-3938a1cf-d097-473e-8e27-47d34c2e8e4b\/index.html",
@@ -516,7 +516,7 @@ var precacheConfig = [
     ],
     [
         "site.json",
-        "Xmv8wBeMs7BF1zt9nK6rIA"
+        "LeMeikQlzSVhigQXirNiA"
     ],
     [
         "404.html",
