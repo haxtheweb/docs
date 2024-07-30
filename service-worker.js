7920df7b17c81035af8c09506ce6ecae452fa7fa
www-data <www-data@hax.vmhost.psu.edu>
2024-07-30 15:49:20 -0400

    Page updated: Peter Aeschbacher Exhibit (item-0ae1c6fb-3f57-4917-8caa-91853eda4982)

--- service-worker.js
+++ service-worker.js
@@ -136,7 +136,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-0ae1c6fb-3f57-4917-8caa-91853eda4982\/index.html",
-        "9LJMyLfolWtfHroN7i0Sg"
+        "HHFKArfg0zaaPn6jhQVAdg"
     ],
     [
         "pages\/item-73d866cd-ebd0-4124-ad99-b1b18455327d\/index.html",
@@ -524,7 +524,7 @@ var precacheConfig = [
     ],
     [
         "site.json",
-        "6oAcdXw28Lab1dWXsUEw"
+        "ffLsnhv9VaFQOxVRELMdw"
     ],
     [
         "404.html",
