4979bc3f692e3f25388170e977efe0ed20c1c7b6
www-data <www-data@hax.vmhost.psu.edu>
2024-08-02 13:59:11 -0400

    Page updated: Ben Andrew Exhibit (item-c47bf439-54fa-4c41-8f39-a72dc0d40b60)

--- service-worker.js
+++ service-worker.js
@@ -144,7 +144,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-c47bf439-54fa-4c41-8f39-a72dc0d40b60\/index.html",
-        "n1ijcwqYB2u9NnJbNi7tQ"
+        "01JX2iHJ9PXTH8OvnHtVw"
     ],
     [
         "pages\/item-e5e58d0e-aa0b-4749-a9cb-e791b08d4105\/index.html",
