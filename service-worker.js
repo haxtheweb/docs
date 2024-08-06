6cdc286f0df4e907e225a43023cab4032e2d2c3f
www-data <www-data@hax.vmhost.psu.edu>
2024-08-06 16:33:59 -0400

    Page updated: Design for Active Learning (item-a54cdc47-2f1c-4d82-8baf-fce180194a81)

--- service-worker.js
+++ service-worker.js
@@ -176,7 +176,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-a54cdc47-2f1c-4d82-8baf-fce180194a81\/index.html",
-        "DpQw95mwUCZZ5CLORpmtQ"
+        "p1k2w0aR5OBpN6Umt1kpA"
     ],
     [
         "pages\/item-b2b17986-f13e-4d6e-93a6-6bd21c930c3c\/index.html",
