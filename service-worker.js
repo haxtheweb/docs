baf8668a7bca4239aa2da274feb97c62e3047210
www-data <www-data@hax.vmhost.psu.edu>
2024-08-15 14:20:42 -0400

    Page updated: In Short: A Sound Process (item-e5e58d0e-aa0b-4749-a9cb-e791b08d4105)

--- service-worker.js
+++ service-worker.js
@@ -148,7 +148,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-e5e58d0e-aa0b-4749-a9cb-e791b08d4105\/index.html",
-        "W8GGd0vtUElCPTEughBFw"
+        "9O2AucvZNPdsXsvfJ55Nw"
     ],
     [
         "pages\/item-a1b1587a-b40a-463f-ad4a-e5a05a98fd8c\/index.html",
