16eff3ec0d944366dc6a7b008ad2c6a9b2bfcbdd
www-data <www-data@hax.vmhost.psu.edu>
2024-08-26 17:02:21 -0400

    Page updated: Effective Educational Video (item-51b1acee-9e5c-4756-998e-d6f3ba97882c)

--- service-worker.js
+++ service-worker.js
@@ -204,7 +204,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-51b1acee-9e5c-4756-998e-d6f3ba97882c\/index.html",
-        "EuiM2rnQ1RLLVi9JW9UNpA"
+        "2GZ4OBXosbAW2cjEk47ONg"
     ],
     [
         "pages\/item-d7a7736b-3262-4d0e-8df7-a9af7823a2b7\/index.html",
