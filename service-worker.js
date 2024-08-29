5060b73f6cd2c6fab22df00920a5fc0d7756aa3d
www-data <www-data@hax.vmhost.psu.edu>
2024-08-29 16:14:25 -0400

    Page updated: Recording the Video (item-adb4f6d7-5f7a-4620-ad3a-b0ff14343e60)

--- service-worker.js
+++ service-worker.js
@@ -236,7 +236,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-adb4f6d7-5f7a-4620-ad3a-b0ff14343e60\/index.html",
-        "D2wQyIPLTPOtKyAoKzL8Q"
+        "OEsrnc7UaM2DNS9QVwXQEg"
     ],
     [
         "pages\/item-e115dd47-3a78-4e40-963e-46466a268290\/index.html",
