ad51453de63f045f0337dfe9e765c7e51ea3e94a
www-data <www-data@hax.vmhost.psu.edu>
2024-08-29 15:03:45 -0400

    Page updated: In Short: Design Guidelines and Principles (item-e115dd47-3a78-4e40-963e-46466a268290)

--- service-worker.js
+++ service-worker.js
@@ -240,7 +240,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-e115dd47-3a78-4e40-963e-46466a268290\/index.html",
-        "u4ytFxr4YIruQfd1YOsGJQ"
+        "6w5xC8e5XdcwgaB9vkQ"
     ],
     [
         "pages\/item-de9dbbb5-949b-49c0-95ad-dbbc84835db0\/index.html",
