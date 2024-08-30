2ecf8e04e063289844c85dd6f5db4c2dd526f74d
www-data <www-data@hax.vmhost.psu.edu>
2024-08-30 10:55:31 -0400

    Page updated: Cognitive Load Theory Primer (item-13056366-d039-4d51-b3ae-a3fcd12cd107)

--- service-worker.js
+++ service-worker.js
@@ -224,7 +224,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-13056366-d039-4d51-b3ae-a3fcd12cd107\/index.html",
-        "PMct9gSgQMyr6GS8YCBag"
+        "RutNGTU75iUh8T1V8M2xLw"
     ],
     [
         "pages\/item-3f3cc6fc-18c9-47cb-95bf-0295b6a9e82d\/index.html",
@@ -596,7 +596,7 @@ var precacheConfig = [
     ],
     [
         "site.json",
-        "QI8Spj65SMNygDIOvfjbA"
+        "vSzdDsOYsbWhYy67omPA"
     ],
     [
         "404.html",
