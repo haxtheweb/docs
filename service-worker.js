e6671a46c5687bc4750929b2495424c8951cc76d
www-data <www-data@hax.vmhost.psu.edu>
2024-10-17 14:55:17 -0400

    Page updated: Bonj Szczygiel Exhibit (item-73d866cd-ebd0-4124-ad99-b1b18455327d)

--- service-worker.js
+++ service-worker.js
@@ -232,7 +232,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-73d866cd-ebd0-4124-ad99-b1b18455327d\/index.html",
-        "1un0bMeOWZzbltUpNvDAVg"
+        "GqTHXkLKfV6kUlfd9ZWRw"
     ],
     [
         "pages\/item-c47bf439-54fa-4c41-8f39-a72dc0d40b60\/index.html",
