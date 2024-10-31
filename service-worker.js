8822373ff97fec1262ddfabf54c63a104a634abf
www-data <www-data@hax.vmhost.psu.edu>
2024-10-31 13:03:07 -0400

    Page updated: How does HAX work? (item-58846489-4cbe-496e-8330-444fa9c69252)

--- service-worker.js
+++ service-worker.js
@@ -60,7 +60,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-58846489-4cbe-496e-8330-444fa9c69252\/index.html",
-        "HCI6IXH6ssVbUO8Mpi0QA"
+        "dh6OKR6q82dR5zTEh7dZw"
     ],
     [
         "pages\/item-96fcae63-7aa1-4bd5-a847-e8b2a49de877\/index.html",
