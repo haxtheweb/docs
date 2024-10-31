2557b7ac0bfdf12daf192b03849422a372a71d0f
www-data <www-data@hax.vmhost.psu.edu>
2024-10-31 14:36:16 -0400

    Page updated: Welcome (item-8a5bf844-d498-4f75-bf96-110eaedb42d9)

--- service-worker.js
+++ service-worker.js
@@ -40,7 +40,7 @@
 var precacheConfig = [
     [
         "pages\/item-8a5bf844-d498-4f75-bf96-110eaedb42d9\/index.html",
-        "aDIrRO7Emt26Wda0uEgQ"
+        "AfO2RrLpg64CQimSG8Xdpw"
     ],
     [
         "pages\/item-4550bc1f-bef7-4891-85f0-58647624a26b\/index.html",
