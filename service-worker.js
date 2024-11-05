5e6ff94a212bbccab56e266f5177e26c37cc2b9b
www-data <www-data@hax.vmhost.psu.edu>
2024-11-05 13:31:14 -0500

    Page updated: HAX Explained and Shown (item-58846489-4cbe-496e-8330-444fa9c69252)

--- service-worker.js
+++ service-worker.js
@@ -60,7 +60,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-58846489-4cbe-496e-8330-444fa9c69252\/index.html",
-        "KF4Rbrqn29rEiVhcV1k0Yw"
+        "BcvlY1ieLJNAlkPmK4QdQ"
     ],
     [
         "pages\/item-d673110e-8d91-4294-b80f-74a38cf51533\/index.html",
