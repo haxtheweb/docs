3519c37a1d7d22dfa633a90efb4cca138902c7d3
www-data <www-data@hax.vmhost.psu.edu>
2024-11-05 14:42:21 -0500

    Page updated: Page (item-4b504f9b-4efd-46a1-81a6-ec67c349c835)

--- service-worker.js
+++ service-worker.js
@@ -64,7 +64,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-4b504f9b-4efd-46a1-81a6-ec67c349c835\/index.html",
-        "YMTiOzwXUV12lbtEpARBxw"
+        "BLuIHYtTcKTfupenwL3Q5Q"
     ],
     [
         "pages\/item-d673110e-8d91-4294-b80f-74a38cf51533\/index.html",
