975b9049573e6ba42bc41b0af1caf9f5cb2097df
www-data <www-data@hax.vmhost.psu.edu>
2024-11-05 14:35:57 -0500

    Page updated: Page (item-4b504f9b-4efd-46a1-81a6-ec67c349c835)

--- service-worker.js
+++ service-worker.js
@@ -64,7 +64,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-4b504f9b-4efd-46a1-81a6-ec67c349c835\/index.html",
-        "gToK4qUXyYLvFYwI9BCg"
+        "1AjgYl6ffh6huZk8ZHPNxA"
     ],
     [
         "pages\/item-d673110e-8d91-4294-b80f-74a38cf51533\/index.html",
