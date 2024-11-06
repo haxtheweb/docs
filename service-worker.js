e23248b14e1c6bde2dec88e9f3413eca50ebcc21
www-data <www-data@hax.vmhost.psu.edu>
2024-11-06 11:29:11 -0500

    Page updated: HiTech Podcast: HAX Explained and Shown (item-4b504f9b-4efd-46a1-81a6-ec67c349c835)

--- service-worker.js
+++ service-worker.js
@@ -60,7 +60,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-4b504f9b-4efd-46a1-81a6-ec67c349c835\/index.html",
-        "oUfD4vPH3nAGu9XZjQE9uA"
+        "cdJeOsBFWzgFh2lNNpvdg"
     ],
     [
         "pages\/item-d673110e-8d91-4294-b80f-74a38cf51533\/index.html",
