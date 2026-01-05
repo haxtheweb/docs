a4fc14c387920e824891045c9258ab3ccf432a80
www-data <www-data@hax.vmhost.psu.edu>
2026-01-05 12:13:26 -0500

    Page updated: HAX Lab (item-cd65aa07-4bab-4f8f-8ffc-c2184f49ce9a)

--- service-worker.js
+++ service-worker.js
@@ -636,7 +636,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-cd65aa07-4bab-4f8f-8ffc-c2184f49ce9a\/index.html",
-        "UOaBKrcVX5SEptJMa63Ndg"
+        "UxGkekWp0iyn4XRTnVMcLg"
     ],
     [
         "pages\/item-861b84da-33c7-49e8-bf25-7aa8d9206d3b\/index.html",
